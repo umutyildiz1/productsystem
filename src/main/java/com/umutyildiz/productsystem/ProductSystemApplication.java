fed63b5f5f4e8f4e1296cc3f809b07d1b1fd51ba
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2022-08-30 12:52:58 +0300

    Project structure is created

---
+++ src/main/java/com/umutyildiz/productsystem/ProductSystemApplication.java
@@ -0,0 +1,13 @@
+package com.umutyildiz.productsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSystemApplication.class, args);
+	}
+
+}
